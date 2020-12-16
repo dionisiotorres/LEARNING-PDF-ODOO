28d2e30b9eecff775128c4ec37e77b968f5e7e17
Javier, Posada <jpo@subteno-it.com>
2020-12-16 16:42:04 +0100

    more pdf

---
+++ lock.sh
@@ -0,0 +1,9 @@
+#!/bin/bash -       
+#title           :lock.sh
+#description     :This script will lock your screen on RaspPi.
+#author		 :Serkikafesa
+#date            :20201211
+#version         :0.1    
+#usage		 :bash lock.sh
+
+/usr/bin/dm-tool lock
